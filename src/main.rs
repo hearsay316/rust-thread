e88e0a2912c933f1aecaaff350126ab170fb37f1
hearsay316 <hearsay316@gmail.com>
2024-04-25 09:53:19 +0800

    feat: cli  开始从命令行中获取参数

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, name = "cli")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Parser)]
+enum Commands {
+    #[command(name = "csv", about = "转化成Json")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long)]
+    input: String,
+
+    #[arg(short, long, default_value = "output.json")]
+    output: String,
+
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+
+    #[arg(long, default_value_t = true)]
+    header: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Cli::parse();
+
+    println!("{:?}", opts)
+
+    // Continued program logic goes here...
 }
