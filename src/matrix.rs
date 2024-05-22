d4d8160cdc95366db3c0bd8e0bb8035b9f0a3b2d
hearsay316 <hearsay316@gmail.com>
2024-05-22 17:43:18 +0800

    feat:  拆分函数, 改成并发

--- src/matrix.rs
+++ src/matrix.rs
@@ -6,24 +6,48 @@ use anyhow::{Result, anyhow};
 use crate::Vector;
 
 const NUM_THREADS: usize = 4;
+
 pub struct Matrix<T: Debug> {
     data: Vec<T>,
     row: usize,
     col: usize,
 }
- pub struct MsgInput<T>{
-     idx:usize,
-     row:Vector<T>,
-     col:Vector<T>,
- }
-pub struct MsgOutput<T>{
-    idx:usize,
-    value:T
+
+pub struct MsgInput<T> {
+    idx: usize,
+    row: Vector<T>,
+    col: Vector<T>,
 }
-pub struct Msg <T> {
+
+impl<T> MsgInput<T> {
+    fn new(idx: usize, row: Vector<T>, col: Vector<T>) -> Self {
+        MsgInput {
+            idx,
+            row,
+            col,
+        }
+    }
+}
+
+pub struct MsgOutput<T> {
+    idx: usize,
+    value: T,
+}
+
+pub struct Msg<T> {
     input: MsgInput<T>,
-    sender: oneshot::Sender<MsgOutput<T>>
+    sender: oneshot::Sender<MsgOutput<T>>,
 }
+
+impl<T> Msg<T> {
+    fn new(input: MsgInput<T>, sender: oneshot::Sender<MsgOutput<T>>) -> Self {
+        Msg {
+            input,
+            sender,
+        }
+    }
+}
+
 impl<T: Debug> Matrix<T> {
     pub fn new(data: impl Into<Vec<T>>, row: usize, col: usize) -> Self {
         Self {
@@ -68,26 +92,46 @@ pub fn dot_product<T>(a: Vector<T>, b: Vector<T>) -> Result<T>
 
 // where 关键字 对泛型做条件限制
 pub fn multiply<T>(a: &Matrix<T>, b: &Matrix<T>) -> Result<Matrix<T>>
-    where T: Debug + Default + Copy + Add<Output=T> + AddAssign + Mul<Output=T>, {
+    where T: Debug + Default + Copy + Add<Output=T> + AddAssign + Mul<Output=T> + Send + 'static, {
     if a.col == b.row {
         return Err(anyhow!("error"));
     }
-   let senders = (0..NUM_THREADS).map(|_| {
-       let (tx,rx) = mpsc::channel();
-       thread::spawn(move |x| {
-
-       })
-   });
-
-    let mut data = vec![T::default(); a.row * b.col];
-
+    let senders = (0..NUM_THREADS).map(|_| {
+        let (tx, rx) = mpsc::channel::<Msg<T>>();
+        thread::spawn(move || {
+            for msg in rx {
+                let value = dot_product(msg.input.row, msg.input.col)?;
+                if let Err(e) = msg.sender.send(MsgOutput { idx: msg.input.idx, value }) {
+                    eprintln!("Send error :{:?}", e);
+                };
+            }
+            Ok::<_, anyhow::Error>(())
+        });
+        tx
+    }).collect::<Vec<_>>();
+    let matrix_len = a.row * b.col;
+    let mut data = vec![T::default(); matrix_len];
+    let mut receivers = Vec::with_capacity(matrix_len);
     for i in 0..a.row {
         for j in 0..b.col {
-                let row = Vector::new(&a.data[i * a.col..(i+1) * a.col]);
-                let col_data = b.data[j..].iter().step_by(b.col).copied().collect::<Vec<_>>();
-                let col = Vector::new(col_data);
-                data[i * b.col + j] += dot_product(row, col)?;
+            let row = Vector::new(&a.data[i * a.col..(i + 1) * a.col]);
+            let col_data = b.data[j..].iter().step_by(b.col).copied().collect::<Vec<_>>();
+            let col = Vector::new(col_data);
+            let idx = i * b.col + j;
+            let input = MsgInput::new(idx, row, col);
+            let (tx, rx) = oneshot::channel();
+            let msg = Msg::new(input, tx);
+            if let Err(e) = senders[idx % NUM_THREADS].send(msg) {
+                eprintln!("sender Err{:?}", e);
+            };
+            // let ret = rx.revc()?;
+            // data[ret.idx] = ret.value;
+            receivers.push(rx);
         }
+    }
+    for rx in receivers {
+        let ret = rx.recv()?;
+        data[ret.idx] = ret.value;
     }
     Ok(Matrix {
         data,
@@ -108,11 +152,12 @@ mod tests {
         println!("{:?}", c);
         Ok(())
     }
+
     #[test]
     fn test_dot_product() -> Result<()> {
         let a = Vector::new(vec![1, 2, 3]);
         let b = Vector::new(vec![4, 5, 6]);
-        let c = dot_product(a,b);
+        let c = dot_product(a, b);
         println!("{:?}", c.unwrap());
         Ok(())
     }
